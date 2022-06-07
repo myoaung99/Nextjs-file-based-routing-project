7fb062471cd2cd87da7a8e229cc9894f563b0a64
MyoMyintAung <myomyintaung064@gmail.com>
2022-06-07 12:32:39 +0630

    added generic head config on root _app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Events For You</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="NextJs Events" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
